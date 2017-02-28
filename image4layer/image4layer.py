f359eb2991229b337f7cc820f1d046f2dac880b4
056-kusakabe-n <n.kusakabe@e-zero1.co.jp>
2017-02-28 20:18:08 +0900

    bug fix

--- image4layer/image4layer.py
+++ image4layer/image4layer.py
@@ -20,6 +20,12 @@ class Image4Layer(object):
         """
         return cls._do_layer(img1, img2, cls._over_lay)
 
+    @staticmethod
+    def _over_lay(a, b):
+        _cl = 2 * a * b / 255
+        _ch = 2 * (a + b - a * b / 255) - 255
+        return _cl * (a < 128) + _ch * (a >= 128)
+
     @classmethod
     def soft_light(cls, img1, img2):
         """
@@ -28,6 +34,12 @@ class Image4Layer(object):
         :type img2: Image
         """
         return cls._do_layer(img1, img2, cls._soft_light)
+
+    @staticmethod
+    def _soft_light(a, b):
+        _cl = (a / 255) ** ((255 - b) / 128) * 255
+        _ch = (a / 255) ** (128 / b) * 255
+        return _cl * (b < 128) + _ch * (b >= 128)
 
     @classmethod
     def hard_light(cls, img1, img2):
@@ -38,24 +50,10 @@ class Image4Layer(object):
         """
         return cls._do_layer(img1, img2, cls._hard_light)
 
-    @classmethod
-    def vivid(cls, img1, img2):
-        """
-        1 - (1 - base) / (2 * ref)    | if ref < 0.5
-        base / (1 - 2 * (ref - 0.5))  | otherwise
-
-        :rtype: Image
-        :type img1: Image
-        :type img2: Image
-        """
-        return cls._do_layer(img1, img2, cls._vivid)
-
     @staticmethod
-    def _vivid(a, b):
-        fa = a / 255.0
-        fb = b / 255.0
-        _cl = (1.0 - ((1.0 - fa) / (2 * fb))) * 255.0
-        _ch = (fa / (1.0 - (2.0 * (fb - 0.5)))) * 255.0
+    def _hard_light(a, b):
+        _cl = 2 * a * b / 255
+        _ch = 2.0 * (a + b - a * b / 255.0) - 255.0
         return _cl * (b < 128) + _ch * (b >= 128)
 
     @classmethod
@@ -92,19 +90,71 @@ class Image4Layer(object):
         return a + b - ((2.0 * a * b) / 255.0)
 
     @classmethod
+    def color_burn(cls, img1, img2):
+        """
+        1 - (1 - base) / ref
+
+        :rtype: Image
+        :type img1: Image
+        :type img2: Image
+        """
+        return cls._do_layer(img1, img2, cls._color_burn)
+
+    @staticmethod
+    def _color_burn(a, b):
+        zero_area = (b == 0)
+        fa = a / 255.0
+        fb = b / 255.0
+        return (1.0 - ((1.0 - fa) / fb)) * 255.0 + (zero_area * 255)
+
+    @classmethod
+    def color_dodge(cls, img1, img2):
+        """
+        base / (1 - ref)
+
+        :rtype: Image
+        :type img1: Image
+        :type img2: Image
+        """
+        return cls._do_layer(img1, img2, cls._color_dodge)
+
+    @staticmethod
+    def _color_dodge(a, b):
+        zero_area = (b == 255)
+        dodge = (a / (255 - b)) * 255.0
+        return dodge + (zero_area * 255)
+
+    @classmethod
     def pin_light(cls, img1, img2):
         """
         :rtype: Image
         :type img1: Image
         :type img2: Image
         """
-        return cls._do_layer(img1, img2, cls._pin_light)
+        return Image4Layer._do_layer(
+            img1, img2,
+            None,
+            "min(a, 2 * b) * (b < 128) + max(a, 2 * (b - 128)) * (b >= 128)"
+        )
+
+    @classmethod
+    def vivid(cls, img1, img2):
+        """
+        1 - (1 - base) / (2 * ref)    | if ref < 0.5
+        base / (1 - 2 * (ref - 0.5))  | otherwise
+
+        :rtype: Image
+        :type img1: Image
+        :type img2: Image
+        """
+
+        return cls._do_layer(img1, img2, cls._vivid)
 
     @staticmethod
-    def _pin_light(a, b):
-        _cl = min(a, 2.0 * b)
-        _ch = max(a, 2.0 * (b - 128))
-        return _cl * (b < 128) + _ch * (b >= 128)
+    def _vivid(a, b):
+        color_burn = Image4Layer._color_burn(a, b * 2)
+        color_dodge = Image4Layer._color_dodge(a, 2 * (b - 128))
+        return color_burn * (b < 128) + color_dodge * (b >= 128)
 
     @staticmethod
     def difference(img1, img2):
@@ -135,20 +185,21 @@ class Image4Layer(object):
         return ImageChops.darker(img1, img2)
 
     @staticmethod
-    def _do_layer(img1, img2, func):
-        eval_str = "func(float(a), float(b))"
+    def _do_layer(img1, img2, func, eval_str="func(float(a), float(b))"):
         num_bands = len(img1.getbands())
 
         if num_bands > 1:
             bands = []
-            for a, b in zip(img1.split(), img2.split()):
+            for a, b in Image4Layer._band_pair(img1, img2):
                 bands.append(ImageMath.eval(eval_str, func=func, a=a, b=b).convert("L"))
 
             if len(bands) < num_bands:
                 bands += img1.split()[len(bands):]
 
             img = Image.merge(img1.mode, bands)
-            if img2.mode == "RGBA" or img2.mode == "LA":
+
+            # img2 has alpha
+            if Image4Layer._check_alpha(img2):
                 base_img = img1.copy()
                 base_img.paste(img, mask=img.split()[-1])
                 img = base_img
@@ -159,26 +210,19 @@ class Image4Layer(object):
 
     @staticmethod
     def _band_pair(img1, img2):
-        if img1.mode == img2.mode:
-            if img1.mode ==
-            for x in zip(img1.split(), img2.split()):
-                yield x
+        bands = []
+        for img in (img1, img2):
+            if Image4Layer._check_alpha(img):
+                bands.append(img.split()[:-1])
+            else:
+                bands.append(img.split())
+
+        b1_len = len(bands[0])
+        b2_len = len(bands[1])
+        for i in range(b1_len):
+            yield bands[0][i], bands[1][min(b2_len-1, i)]
 
     @staticmethod
-    def _over_lay(a, b):
-        _cl = 2 * a * b / 255
-        _ch = 2 * (a + b - a * b / 255) - 255
-        return _cl * (a < 128) + _ch * (a >= 128)
-
-    @staticmethod
-    def _soft_light(a, b):
-        _cl = (a / 255) ** ((255 - b) / 128) * 255
-        _ch = (a / 255) ** (128 / b) * 255
-        return _cl * (b < 128) + _ch * (b >= 128)
-
-    @staticmethod
-    def _hard_light(a, b):
-        _cl = 2 * a * b / 255
-        _ch = 2.0 * (a + b - a * b / 255.0) - 255.0
-        return _cl * (b < 128) + _ch * (b >= 128)
+    def _check_alpha(img):
+        return img.mode in ("RGBA", "LA")
 
--- tests/test_image4layer.py
+++ tests/test_image4layer.py
@@ -16,4 +16,38 @@ def test_layer():
     Image4Layer.over_lay(img1, a_img)
     Image4Layer.over_lay(a_img, img1)
 
+    Image4Layer.color_burn(img1, img2)
+    Image4Layer.color_dodge(img1, img2)
+    Image4Layer.vivid(img1, img2)
+
+    Image4Layer.darken(img1, img2)
+    Image4Layer.lighten(img1, img2)
     Image4Layer.pin_light(img1, img2)
+
+
+def test_mode_pair():
+    img1 = Image.open("sample.png")
+    img2 = Image.open("effect.png")
+    a_img = Image.open("alpha.png")
+
+    # L &  L
+    Image4Layer.over_lay(img1.convert("L"), img2.convert("L"))
+    # RGB & L
+    Image4Layer.over_lay(img1, img2.convert("L"))
+    # RGBA & L
+    img = Image4Layer.over_lay(a_img, img2.convert("L"))
+    assert img.mode == "RGBA"
+    # HSV & L
+    Image4Layer.over_lay(img1.convert("HSV"), img2.convert("L"))
+    # CMYK & L
+    Image4Layer.over_lay(img1.convert("CMYK"), img2.convert("L"))
+
+    # L & RGB
+    img = Image4Layer.over_lay(img2.convert("L"), img1.convert("RGB"))
+    assert img.mode == "L"
+
+
+
+
+
+
